376a1f1c154e49e1a039ffc2ade9eb425101ad26
Lord.Doom <70491204+Doominicprog@users.noreply.github.com>
2020-08-31 05:16:50 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord,asyncio,youtube_dl
+from discord.ext import commands
+import os
+
+
+
+
+def get_prefix(bot, msg):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    prefixes = ['+'] #Your bot prefix(s)
+
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
+
+bot=commands.Bot(command_prefix=get_prefix,description='Multipurpose Discord Bot')
+
+
+
+
+exts=['music'] #Add your Cog extensions here
+
+
+@bot.event
+async def on_ready():
+    song_name='TWICE - What is love?' 
+    activity_type=discord.ActivityType.listening
+    await bot.change_presence(activity=discord.Activity(type=activity_type,name=song_name))
+    print(bot.user.name)
+
+
+
+
+
+
+for i in exts:
+    bot.load_extension(i)
+
+
+bot.run('NzQzMjQzNjI3ODUzMjUwNjUx.XzR1kQ.7aymthCbQ8Uv023_1ZEQ2wq97Jg')
